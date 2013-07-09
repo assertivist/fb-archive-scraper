b024690d0686568245fa4412e2f6bba65f2a8d39
assertivist <croc+github@sdf.org>
2013-07-09 16:10:59 -0500

    fixed subdirectory problem, pass entire archive directory into script instead of single page, updated readme

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 fb-archive-scraper
 ==================
 
-A script to download all assets from a Facebook archive so that they are locally stored 
+I was really excited when Facebook began offering a service to download all your content in an archive, they were finally doing a Good Thing!
+
+After downloading and inspecting the archive, I was quite pleased to see that much of the data that is 'behind the scenes' was included beyond the content that I uploaded myself.
+
+Fast forward a couple months, and I'm on my laptop with no internet connection. I wanted to show a (real life, not Facebook) friend of mine a picture that I had uploaded a year or so ago. Of course, this MUST be in my archive! I fire up my web browser and point it to the index of the archive only to find...
+
+==The Photos Weren't There==
+... a bunch of broken images! Facebook had, for some reason, chose to include only a small amount of my photos in my content archive. This, to me, was unnacceptable; if you're going to offer a "download all your content" button, you had better make sure it downloads all your content!
+
+The reason that Facebook pulls this nonsense even today escapes me. 
+
+So I wrote a simple script to "fix" this problem. What it does is go through your archive and scrapes all the remote content that facebook put in your archive to be local content, and then updates the html files to point to the local content instead. There you go, a REAL archive of your Facebook photos and statuses, not dependent on any internet connection when browsing locally!
+
+How to use
+==========
+
+`python fb-archive-scraper <path to archive>`
+
+This will create a subfolder in each photo album folder called "scraped", and save the remote photos there. It also makes sure there are no remote files on any of the other archive pages, and creates a similar 'scraped' folder in the html section of your archive as well.
+
+Limited Warranty
+================
+
+Always use a copy of your archive to run the script on, I'm not responsible for any damages caused by using this script.
+
+This software is available AS IS and AT RISK!
--- fb-archive-scraper.py
+++ fb-archive-scraper.py
@@ -1,51 +1,71 @@
 from os.path import basename
 from urlparse import urlsplit
 from bs4 import BeautifulSoup
-import urllib2
 import os
+import errno 
+import sys
+import urllib2
 
-def scrape(profile_index_local_file, currdir):
-    with open(profile_index_local_file) as f:
+def scrape(html_file):
+    with open(html_file) as f:
         html = f.read()
         soup = BeautifulSoup(html)
-        #print(soup.prettify())
+        html_dir = os.path.split(html_file)[0]
         for img in soup.find_all('img'):
             src = img.get('src')
-            if '..' not in src:
+            if 'http' in src: #get all remote images
                 print src
-                elems = src.split('/')
-                filename = elems[-1]
-                download(src, filename)
-                print filename
-                newfilename = "%s/stolen/%s" % (currdir,filename)
-                print newfilename
-                img['src'] = newfilename
-        print soup.prettify()
+                elems = src.split('/') 
+                filename = elems[-1] #image name
+                new_path = os.path.join(os.path.join(html_dir, 'scraped/'), filename)
+                new_dir = os.path.split(new_path)[0]
+                make_sure_path_exists(new_dir)
+                download(src, new_path) #save it
+                print "got ", filename
+                newfilename = os.path.relpath(new_path, html_dir) #"stolen/%s" % (filename)
+                img['src'] = newfilename #change the src in img tag
+
+    with open(html_file, 'w+') as f:
+        f.write(soup.prettify().encode('ascii', 'xmlcharrefreplace')) #save html
 
 def url2name(url):
     return basename(urlsplit(url)[2])
 
-def download(url, localFileName = None):
+def download(url, localFileName):
     localName = url2name(url)
     req = urllib2.Request(url)
     r = urllib2.urlopen(req)
-    if r.info().has_key('Content-Disposition'):
-        # If the response has Content-Disposition, we take file name from it
-        localName = r.info()['Content-Disposition'].split('filename=')[1]
-        if localName[0] == '"' or localName[0] == "'":
-            localName = localName[1:-1]
-    elif r.url != url: 
-        # if we were redirected, the real file name we take from the final URL
-        localName = url2name(r.url)
-    if localFileName: 
-        # we can force to save the file as specified name
-        localName = localFileName
-    fullpath = os.path.abspath("facebook-assertivist/stolen/%s" % localName)
-    f = open(fullpath, 'w+')
-    f.write(r.read())
-    f.close()
+    with open(localFileName, 'w+') as f:
+        f.write(r.read())
 
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
 
 if __name__ == "__main__":
-    print("running")
-    scrape('facebook-assertivist/html/photos.htm')
+    try:
+        main_dir = sys.argv[1]
+    except:
+        print "usage: fb-archive-scraper.py <path to fb archive>"
+
+    photos = os.path.join(main_dir, 'photos')
+    if os.path.exists(photos):
+        for filename in os.listdir(photos):
+            full_fname = os.path.join(photos, filename)
+            if os.path.isdir(full_fname):
+                print "scraping photo album with id: ", filename
+                albumdir = os.path.join(photos, full_fname)
+                albumindex = os.path.join(albumdir, 'index.htm')
+                if os.path.isfile(albumindex):
+                    scrape(albumindex)
+
+    html_dir = os.path.join(main_dir, 'html')
+    if os.path.exists(html_dir):
+        for filename in os.listdir(html_dir):
+            full_fname = os.path.join(html_dir, filename)
+            if os.path.isfile(full_fname) and '.htm' in filename:
+                print "scraping for ", filename
+                scrape(full_fname)
